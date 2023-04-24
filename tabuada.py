13be568eefadc8e54dd200ba10d5d48b20f8ef75
MILENA VEIGA <47834751+MiVeiga@users.noreply.github.com>
2023-04-24 20:29:29 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+# Faça um programa que leia um número inteiro qualquer
+# e mostre na tela a sua tabuada
+
+n = int(input("Digite um número \n"))
+print("-"*12)
+print("{} * {} = {}".format(n, 1, n*1))
+print("{} * {} = {}".format(n, 2, n*2))
+print("{} * {} = {}".format(n, 3, n*3))
+print("{} * {} = {}".format(n, 4, n*4))
+print("{} * {} = {}".format(n, 5, n*5))
+print("{} * {} = {}".format(n, 6, n*6))
+print("{} * {} = {}".format(n, 7, n*7))
+print("{} * {} = {}".format(n, 8, n*8))
+print("{} * {} = {}".format(n, 9, n*9))
+print("-"*12)
